a63bacd254e4da12c759b343bcf0b26b9548bece
Ochaki0714 <133471564+Ochaki0714@users.noreply.github.com>
2025-07-26 17:10:10 +0900

    Initial commit

---
+++ app/(content)/news/[slug]/not-found.jsx
@@ -0,0 +1,8 @@
+export default function NewsNotFoundPage() {
+  return (
+    <div className='error'>
+      <h1>Not Found</h1>
+      <p>Unfortunately, webcould not find the requseted article.</p>
+    </div>
+  );
+}
