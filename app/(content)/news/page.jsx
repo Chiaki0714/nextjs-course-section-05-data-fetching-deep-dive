f8f6e98e47ffe5c96593530e04068b300a648d70
Ochaki0714 <133471564+Ochaki0714@users.noreply.github.com>
2025-07-26 22:32:30 +0900

    Data Fetching: Implemented server-side data fetching (video no.161)

--- app/(content)/news/page.jsx
+++ app/(content)/news/page.jsx
@@ -1,11 +1,20 @@
-import { DUMMY_NEWS } from '@/dummy-news';
 import NewsList from '@/components/news-list';
 
-export default function NewsPage() {
+export default async function NewsPage() {
+  const response = await fetch('http://localhost:8080/news', {
+    next: { revalidate: 60 },
+  });
+
+  if (!response.ok) {
+    throw new Error('Fail to fetch news');
+  }
+
+  const data = await response.json();
+
   return (
     <>
       <h1>News Page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      <NewsList news={data} />
     </>
   );
 }
